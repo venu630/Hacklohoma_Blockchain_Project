7732a6bb62a4b9756160d91a9940b139e0818f8d
Phani Charan <charanphani5@gmail.com>
2025-02-15 15:25:52 -0600

    Phani_FirstCommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect } from 'react';
-import {
-  Routes,
-  Route,
-} from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import Dashboard from './pages/Dashboard';
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
-
   return (
     <>
       <Routes>
         <Route exact path="/" element={<Dashboard />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<SignUp />} />
       </Routes>
     </>
   );
